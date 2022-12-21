3e617cf8cf4b25aa537f35b1883a4c513fcb7eac
sojib-sudo <sojibmandol@gmail.com>
2022-12-21 14:11:20 +0600

    fotter section update

--- src/components/Pages/Shared/Footer/Footer.js
+++ src/components/Pages/Shared/Footer/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div className='text-center mt-5' style={{position: 'relative', left:'0', bottom:'0', right:'0'}}>
+        <div className='text-center  mt-5' style={{position: 'relative', left:'0', bottom:'0', right:'0'}}>
             <p><small>© Star Programming School, 2022. All rights reserved.</small></p>
         </div>
     );
